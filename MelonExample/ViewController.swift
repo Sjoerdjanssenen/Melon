72798903433984684c4b6062a4c9e278593c359c
Sjoerd Janssen <sjoerd.janssen@vimen.nl>
2018-05-27 02:18:07 +0200

    add an example project

---
+++ MelonExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MelonExample
+//
+//  Created by Sjoerd Janssen on 27/05/2018.
+//  Copyright © 2018 Sjoerd Janssen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
